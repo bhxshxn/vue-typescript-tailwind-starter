b954a75a10f8997637606991e62354014c552be9
bhxshxn <bhushanchaudhary3333@gmail.com>
2023-01-14 16:02:27 +0530

    init

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,12 @@
-<template></template>
+<template>
+  <h1>Login</h1>
+</template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "HomeView",
+  name: "Login",
   components: {},
 });
 </script>
